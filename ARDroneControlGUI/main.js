ee7b15bee0405402a005ed3eb694710b5ac8d663
ngpaladi <ngpaladi@gmail.com>
2016-07-13 10:48:12 -0400

    Implemented system to stop drone from continuously flying in one direction on keypress

--- ARDroneControlGUI/main.js
+++ ARDroneControlGUI/main.js
@@ -23,7 +23,7 @@ function createWindow() {
   //arClient.takeoff();
   
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 1000, height: 600});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);
