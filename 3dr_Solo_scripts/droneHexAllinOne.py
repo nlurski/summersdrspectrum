6ba4d009f5bdc2749571cf4220f7d6c1b3b12850
Nick Lurski <nlurski@gmail.com>
2016-08-02 11:30:01 -0400

    timestamp fix

--- 3dr_Solo_scripts/droneHexAllinOne.py
+++ 3dr_Solo_scripts/droneHexAllinOne.py
@@ -79,8 +79,7 @@ def runA():
 def runB():
     global run
     while run:
-        #gpsData[time.time()] = str(vehicle.location.global_frame)
-        gpsData[datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')] = str(vehicle.location.global_frame)
+        gpsData[time.time()] = str(vehicle.location.global_frame)
         time.sleep(.1)
     with open('gpsTimestamps', 'w') as f:
         json.dump(gpsData, f)
